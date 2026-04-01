14c14b615ab761931628234a99e9b491f7881b70
kanishka2143 <167493131+kanishka2143@users.noreply.github.com>
2026-04-01 10:02:58 +0530

    Initial commit for new SQL based Spring Boot project

---
+++ src/main/java/com/smartcampus/it3030_paf_2026_smart_campus/It3030Paf2026SmartCampusApplication.java
@@ -0,0 +1,13 @@
+package com.smartcampus.it3030_paf_2026_smart_campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It3030Paf2026SmartCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It3030Paf2026SmartCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartcampus/it3030_paf_2026_smart_campus/It3030Paf2026SmartCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcampus.it3030_paf_2026_smart_campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It3030Paf2026SmartCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
